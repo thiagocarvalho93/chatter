51368358ab989c312c6ea0542fecebb6ce7c13db
thiagocarvalho93 <thiagopaes1993@gmail.com>
2025-05-15 14:39:59 -0300

    adjusting environment variable name

--- client/src/constants/app-constants.js
+++ client/src/constants/app-constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = import.meta?.env?.VITE_API_URL ?? "http://localhost:5014";
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5014";
